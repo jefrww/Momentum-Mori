80585aa3f95dfba588729d63dfc70b8dc2e86abb
Jan-Erik Wieczorek <JanErWi@aol.com>
2020-07-12 01:52:26 +0200

    jumpanimation

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,7 +12,8 @@ var velocity = Vector3()
 onready var collected = 0
 export var winCondition = 100
 
-
+var jumping = false
+var jumpduration = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,6 +21,13 @@ func _ready():
 
 func _process(delta):
 	updateAnim()
+	if jumping:
+		jumpduration += delta
+		print(jumpduration)
+		if jumpduration >= 1:
+			get_child(2).get_child(2).set("parameters/Jump/active", false)
+			jumping = false
+			jumpduration = 0
 
 func updateAnim():
 	var choppedVel = Vector2(velocity.x, velocity.z)
@@ -45,16 +53,16 @@ func player_movement():
 	var moved = false
 	var newVelocity = velocity
 	newVelocity.y = 0
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up") && is_on_floor():
 		newVelocity += -transform.basis.z * acceleration
 		moved = true
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down") && is_on_floor():
 		newVelocity += transform.basis.z * acceleration
 		moved = true
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left") && is_on_floor():
 		newVelocity += -transform.basis.x * acceleration
 		moved = true
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") && is_on_floor():
 		newVelocity += transform.basis.x * acceleration
 		moved = true
 
@@ -63,8 +71,10 @@ func player_movement():
 		
 	if Input.is_action_pressed("ui_accept") && is_on_floor():
 		print("Jump")
+		get_child(2).get_child(2).set("parameters/Jump/active", true)
 		newVelocity.y = jumpPower
 		moved = true
+		jumping = true
 	else:
 		newVelocity.y = velocity.y
 	if !moved:
