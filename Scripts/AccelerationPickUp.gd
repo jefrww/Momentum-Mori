0316057a4051f0e372d16f631324c46b64404769
NaveSucks <51803130+NaveSucks@users.noreply.github.com>
2020-07-12 06:49:08 +0200

    vincents letzte ruhe

--- Scripts/AccelerationPickUp.gd
+++ Scripts/AccelerationPickUp.gd
@@ -7,14 +7,13 @@ func _on_AccelerationPickUp_body_entered(body):
 		body.acceleration *= (1+accelerationIncrease)
 		body._play_coin_sound()
 		body.collected += 1
-		if body.collected >= body.winCondition:
-			get_tree().change_scene("res://Scenes/EndScene.tscn")
+		if body.collected == body.winCondition:
+			var error = get_tree().change_scene("res://Scenes/Win.tscn")
+			if error:
+				print(error)	
 	for child in get_children():
 		child.queue_free()
 	queue_free()
 	print("DESTROYED")
-	if body.collected == body.winCondition:
-		var error = get_tree().change_scene("res://Scenes/Win.tscn")
-		if error:
-			print(error)
+	
 
