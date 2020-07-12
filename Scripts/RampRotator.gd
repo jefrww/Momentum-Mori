087a37e97d4342591e469d90ff0ac06001147fde
Kinghoff <finn-m97@mail.de>
2020-07-12 19:34:07 +0200

    added ramp sound, etc

--- Scripts/RampRotator.gd
+++ Scripts/RampRotator.gd
@@ -3,10 +3,15 @@ extends Spatial
 export var rotationSpeed = - 0.1
 export(NodePath) var player
 onready var preRot = 1
+export(NodePath) var pathToSFX
+onready var soundPlayer = get_node(pathToSFX) 
+onready var soundPlayable = true
 
 func _process(delta):
 	var curRot = transform.basis.z.angle_to(Vector3(0,0,1))
-	if get_node(player).collected >=5 &&  curRot < preRot:
+	if get_node(player).collected >=63 &&  curRot < preRot:
+		if soundPlayable:
+			soundPlayable = false
+			soundPlayer.play(0.0)
 		rotate_x(rotationSpeed*delta)
 		preRot = curRot
-	
