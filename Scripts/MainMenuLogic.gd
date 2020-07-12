f7fbe8452414b6c73574ef6e0a5ff448b16f923b
NaveSucks <51803130+NaveSucks@users.noreply.github.com>
2020-07-12 02:31:02 +0200

    changed starting level

--- Scripts/MainMenuLogic.gd
+++ Scripts/MainMenuLogic.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _on_StartButton_pressed():
 	print("STARTING")
-	var error = get_tree().change_scene("res://Scenes/Level0.tscn")
+	var error = get_tree().change_scene("res://Scenes/Level1.tscn")
 	if error:
 		print(error)
 
