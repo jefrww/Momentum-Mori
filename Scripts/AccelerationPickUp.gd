eb530579d3e67484797d6d7861ed9b26c29eadb3
Kinghoff <finn-m97@mail.de>
2020-07-12 03:59:49 +0200

    jump and coin sounds work now. yay!

--- Scripts/AccelerationPickUp.gd
+++ Scripts/AccelerationPickUp.gd
@@ -5,6 +5,7 @@ export var accelerationIncrease = 0.05
 func _on_AccelerationPickUp_body_entered(body):
 	if body.name == "Player":
 		body.acceleration *= (1+accelerationIncrease)
+		body._play_coin_sound()
 		body.collected += 1
 		if body.collected >= body.winCondition:
 			get_tree().change_scene("res://Scenes/EndScene.tscn")
